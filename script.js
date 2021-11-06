f27ba76adc604bfba4f680e4ee408599d4cb718f
xxx <moldovan.alexandra28@gmail.com>
2021-11-06 20:17:40 +0200

    updating project

--- script.js
+++ script.js
@@ -16,9 +16,7 @@
     var year = new Date().getFullYear()+parseInt(years);
     /* variable rate is created to access the element whose id is "rate" */
     var result = document.getElementById("result");
-    result.innerHTML="If you deposit "+principal +",<br/>"+ " at an interest rate of " +rate +".<br/>" + "You will receive an amount of "+(principal * years * rate /100)+",<br/>" + " in the year "+ year
-
-
+    result.innerHTML = "If you deposit " + "<span class='highlight'>" + principal + "</span>." + ", <br> at an interest rate of " + "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + presentYear + "</span>";
 }
 
 /* function that reads the value of the range slider and displays it the */
